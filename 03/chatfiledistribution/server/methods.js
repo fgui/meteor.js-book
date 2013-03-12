0f0fcadc18e5816cf16343201742b45f610f28b9
francesc <francesc.guillen@gmail.com>
2013-03-12 21:25:54 +0100

    add "var"

--- 03/chatfiledistribution/server/methods.js
+++ 03/chatfiledistribution/server/methods.js
@@ -5,7 +5,7 @@ Meteor.methods({
       throw new Meteor.Error(400, "Some parametres are wrong.");
     if (! this.userId)
       throw new Meteor.Error(403, "You must be logged in");
-    datenow = Date.now();
+    var datenow = Date.now();
     return Messages.insert({
       owner: this.userId,
       name: Meteor.user().emails[0].address,
